11365bbc7a787efb62601cdc0f1ed352c37586bc
Antigrafik <mai.antigrafik@gmail.com>
2025-06-11 16:04:14 +0200

    Add submenu

--- private/modules/intra/views/admin/index.php
+++ private/modules/intra/views/admin/index.php
@@ -1,1 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Usuarios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tienda</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aula</a>
+            </li>
+                        <li class="nav-item">
+                <a class="nav-link" href="#">Foro</a>
+            </li>
+                        <li class="nav-item">
+                <a class="nav-link" href="#">Comunidad</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <p>INTRA ADMIN</p>
