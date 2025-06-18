33c1d1ae7923ca1ac7ee6e1da93f4d6e28720010
Antigrafik <mai.antigrafik@gmail.com>
2025-06-18 11:56:30 +0200

    Add admin section views and update navigation links

--- private/modules/intra/views/admin/admin.php
+++ private/modules/intra/views/admin/admin.php
@@ -1,1 +1,10 @@
-// admin.php
+<h1>Dashboard admin</h1>
+<p>Bienvenido al panel de administración.</p>
+<p>Desde aquí puedes gestionar los usuarios, roles, permisos y otras configuraciones del sistema.</p>
+<p>Utiliza el menú de navegación para acceder a las diferentes secciones.</p>
+<p>Si necesitas ayuda, consulta la documentación o contacta con el soporte técnico.</p>
+<p>Recuerda que los cambios realizados aquí pueden afectar a todo el sistema, así que procede con precaución.</p>
+<p>Para más información, visita la sección de <a href="https://www.example.com/documentacion">documentación</a>.</p>
+<p>Gracias por utilizar nuestro sistema. Esperamos que tengas una excelente experiencia.</p>
+<p>Si tienes alguna sugerencia o comentario, no dudes en hacérnoslo saber.</p>
+<p>¡Disfruta de tu día!</p>
--- private/modules/intra/views/admin/index.php
+++ private/modules/intra/views/admin/index.php
@@ -2,21 +2,52 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Usuarios</a>
+                <a class="nav-link" href="/admin">Inicio</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="?section=users">Usuarios</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Tienda</a>
+                <a class="nav-link" href="?section=store">Tienda</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Aula</a>
+                <a class="nav-link" href="?section=learn">Aula</a>
             </li>
-                        <li class="nav-item">
-                <a class="nav-link" href="#">Foro</a>
+            <li class="nav-item">
+                <a class="nav-link" href="?section=forum">Foro</a>
             </li>
-                        <li class="nav-item">
-                <a class="nav-link" href="#">Comunidad</a>
+            <li class="nav-item">
+                <a class="nav-link" href="?section=community">Comunidad</a>
             </li>
         </ul>
     </div>
 </nav>
-<p>INTRA ADMIN</p>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+$parts = explode('/', trim($request, '/')); // quita slashes y separa
+$section = $parts[1] ?? 'admin'; // [0] sería "admin", [1] sería la sección
+
+switch ($section) {
+    case 'admin':
+        include 'admin.php';
+        break;
+    case 'users':
+        include 'users.php';
+        break;
+    case 'store':
+        include 'store.php';
+        break;
+    case 'learn':
+        include 'learn.php';
+        break;
+    case 'forum':
+        include 'forum.php';
+        break;
+    case 'community':
+        include 'community.php';
+        break;
+    default:
+        echo "<p>Sección no encontrada.</p>";
+        break;
+}
+?>
